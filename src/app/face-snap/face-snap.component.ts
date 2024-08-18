ffe07bb04b6f51501c37ec566c8c8b8b9d8fabd2
Tony Dugué <duguetony@gmail.com>
2024-08-18 11:33:29 +0200

    feat: create face-snap component

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss',
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = "Mon meilleur ami depuis toujours !";
+    this.createdAt = new Date();
+    this.snaps = 0;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+  }
+}
+
