6fb0059b14ea96fa7cd4e22464b018c89823b271
Tony Dugué <duguetony@gmail.com>
2024-08-18 12:01:46 +0200

    feat: add button for add or remove snap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,13 +13,33 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = 'Archibald';
-    this.description = "Mon meilleur ami depuis toujours !";
+    this.description = 'Mon meilleur ami depuis toujours !';
     this.createdAt = new Date();
     this.snaps = 0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = 'Ajouter un snap';
+    this.userHasSnapped = false;
+  }
+
+  onSnap(): void {
+    this.userHasSnapped ? this.removeSnap() : this.addSnap();
+  }
+
+  addSnap(): void {
+    this.snaps++;
+    this.snapButtonText = 'Retirer un snap';
+    this.userHasSnapped = true;
+  }
+
+  removeSnap(): void {
+    this.snaps--;
+    this.snapButtonText = 'Ajoute un snap';
+    this.userHasSnapped = false;
   }
 }
-
