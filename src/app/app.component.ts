846c1ead832c7269f96a5c20b3e87de3ef012b42
Tony Dugué <duguetony@gmail.com>
2024-08-19 02:10:09 +0200

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FaceSnapListComponent],
+  imports: [HeaderComponent, FaceSnapListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
